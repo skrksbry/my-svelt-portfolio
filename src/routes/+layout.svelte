5d558baf91ca304af9ec7df2904af6bb8c5e1d37
Jae Hyun Goh <skrksbry@naver.com>
2024-04-16 01:39:22 +0900

    feat: footer 임시 주석처리, Vercel Analytics 추가

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
-	import Header from './Header.svelte';
+	import { inject } from '@vercel/analytics'
 	import './styles.css';
+
+	inject();
 </script>
 
 <div class="app">
@@ -8,9 +10,9 @@
 		<slot />
 	</main>
 
-	<footer>
+<!--	<footer>-->
 
-	</footer>
+<!--	</footer>-->
 </div>
 
 <style>
@@ -29,13 +31,13 @@
 		box-sizing: border-box;
 	}
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
+	/*footer {*/
+	/*	display: flex;*/
+	/*	flex-direction: column;*/
+	/*	justify-content: center;*/
+	/*	align-items: center;*/
+	/*	padding: 12px;*/
+	/*}*/
 
 	/*@media (min-width: 480px) {*/
 	/*	footer {*/
