94a76e4d798c7105fb88b20391047fcc8048a6e5
Jae Hyun Goh <skrksbry@naver.com>
2024-04-18 01:55:12 +0900

    feat: 블로그 로고 컴포넌트 추가

---
+++ src/components/KirsiBlogLogo.svelte
@@ -0,0 +1,16 @@
+<div class="logo">K</div>
+
+<style>
+    .logo {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 24px;
+        height: 24px;
+        background-color: #eb7575;
+        font-size: 24px;
+        font-weight: 900;
+        color: #000;
+        border-radius: 5px;
+    }
+</style>
